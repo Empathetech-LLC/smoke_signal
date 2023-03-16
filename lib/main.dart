0fc98991b17584c0ad3d2b95b385c6c2cd58a064
empathictech <105748722+Empathictech@users.noreply.github.com>
2023-03-16 16:38:54 -0400

    added placeholders

--- lib/main.dart
+++ lib/main.dart
@@ -65,6 +65,12 @@ class SmokeSignal extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: appTitle,
 
+      // Android app data
+      material: (context, platform) => MaterialAppData(),
+
+      // iOS app data
+      cupertino: (context, platform) => CupertinoAppData(),
+
       // Route data
       initialRoute: homeRoute,
       routes: {
