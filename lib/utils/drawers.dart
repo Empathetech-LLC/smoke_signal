fbd39cbea493c8c017a8895c57c04e5e973daf3f
empathictech <105748722+Empathictech@users.noreply.github.com>
2023-03-24 16:46:46 -0400

    split drawers into parts

--- lib/utils/drawers.dart
+++ lib/utils/drawers.dart
@@ -7,144 +7,135 @@ import 'package:flutter/material.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
 
-Drawer standardDrawer(BuildContext context) {
-  late double buttonSpacer = AppConfig.prefs[buttonSpacingKey];
-
-  return Drawer(
-    backgroundColor: Color(AppConfig.prefs[themeColorKey]),
-    child: ezScrollView(
-      children: [
-        // App icon
-        DrawerHeader(
-          child: CircleAvatar(
-            backgroundImage: AssetImage(appIconPath),
-            minRadius: 50,
-            maxRadius: 50,
-          ),
-        ),
-
-        // GoTo settings
-        ezIconButton(
-          action: () => Navigator.of(context).popAndPushNamed(
-            settingsRoute,
-            arguments: {indexArg: 0},
-          ),
-          icon: ezIcon(PlatformIcons(context).settings),
-          message: 'Settings',
-        ),
-        Container(height: buttonSpacer),
-
-        // Show input rules
-        ezButton(
-          action: () => ezDialog(
-            context: context,
-            title: 'Input rules',
-            content: Text(
-              validatorRule,
-              style: getTextStyle(dialogContentStyleKey),
-              textAlign: TextAlign.center,
-            ),
-          ),
-          body: Text('Input rules'),
-        ),
-        Container(height: buttonSpacer),
-      ],
-    ),
+/// "Standard" drawer header
+/// For use on screen in which settings should be available, but no user is logged in
+Widget standardDrawerHeader() {
+  return CircleAvatar(
+    backgroundImage: AssetImage(appIconPath),
+    minRadius: 50,
+    maxRadius: 50,
   );
 }
 
-Drawer signalBoardDrawer(BuildContext context) {
+/// "Standard" drawer body
+/// For use on screen in which settings should be available, but no user is logged in
+List<Widget> standardDrawerBody(BuildContext context) {
   double buttonSpacer = AppConfig.prefs[buttonSpacingKey];
 
-  return Drawer(
-    backgroundColor: Color(AppConfig.prefs[themeColorKey]),
-    child: ezScrollView(
-      children: [
-        // Header: User profile preview, edit button, logout button
-        DrawerHeader(
-          child: Row(
-            mainAxisSize: MainAxisSize.max,
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              // Profile image and name
-              Column(
-                mainAxisSize: MainAxisSize.max,
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  // Profile image
-                  CircleAvatar(
-                    foregroundImage: CachedNetworkImageProvider(
-                        AppUser.account.photoURL ?? defaultAvatarURL),
-                    minRadius: 50,
-                    maxRadius: 50,
-                  ),
-
-                  // Profile name
-                  Text(
-                    AppUser.account.displayName ?? defaultDisplayName,
-                    style: getTextStyle(dialogTitleStyleKey),
-                  ),
-                ],
-              ),
-
-              // Edit and logout buttons
-              Column(
-                mainAxisSize: MainAxisSize.max,
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  // Edit
-                  ezButton(
-                    action: () =>
-                        Navigator.of(context).popAndPushNamed(profileSettingsRoute),
-                    body: ezIcon(Icons.edit),
-                  ),
-
-                  // Logout
-                  ezButton(
-                    action: () => logout(context),
-                    body: ezIcon(Icons.logout),
-                  ),
-                ],
-              ),
-            ],
-          ),
-        ),
-        Container(height: buttonSpacer),
-
-        // Add new signal
-        ezIconButton(
-          action: () => Navigator.of(context).popAndPushNamed(createSignalRoute),
-          message: 'New',
-          icon: ezIcon(PlatformIcons(context).add),
-        ),
-        Container(height: buttonSpacer),
-
-        // GoTo settings
-        ezIconButton(
-          action: () => Navigator.of(context).popAndPushNamed(
-            settingsRoute,
-            arguments: {indexArg: 1},
-          ),
-          message: 'Settings',
-          icon: ezIcon(PlatformIcons(context).settings),
-        ),
-        Container(height: buttonSpacer),
-
-        // Show input rules
-        ezButton(
-          action: () => ezDialog(
-            context: context,
-            title: 'Input rules',
-            content: Text(
-              validatorRule,
-              style: getTextStyle(dialogContentStyleKey),
-              textAlign: TextAlign.center,
-            ),
-          ),
-          body: Text('Input rules'),
-        ),
-        Container(height: buttonSpacer),
-      ],
+  return [
+    // GoTo settings
+    ezIconButton(
+      action: () => Navigator.of(context).popAndPushNamed(
+        settingsRoute,
+        arguments: {indexArg: 0},
+      ),
+      icon: ezIcon(PlatformIcons(context).settings),
+      message: 'Settings',
     ),
+    Container(height: buttonSpacer),
+
+    // Show input rules
+    ezButton(
+      action: () => ezDialog(
+        context: context,
+        title: 'Input rules',
+        content: Text(
+          validatorRule,
+          style: getTextStyle(dialogContentStyleKey),
+          textAlign: TextAlign.center,
+        ),
+      ),
+      body: Text('Input rules'),
+    ),
+    Container(height: buttonSpacer),
+  ];
+}
+
+/// Custom drawer header for Signal Board
+Widget signalDrawerHeader(BuildContext context) {
+  return Row(
+    mainAxisSize: MainAxisSize.max,
+    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+    children: [
+      // Profile image and name
+      Column(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          // Profile image
+          CircleAvatar(
+            foregroundImage:
+                CachedNetworkImageProvider(AppUser.account.photoURL ?? defaultAvatarURL),
+            minRadius: 50,
+            maxRadius: 50,
+          ),
+
+          // Profile name
+          Text(
+            AppUser.account.displayName ?? defaultDisplayName,
+            style: getTextStyle(dialogTitleStyleKey),
+          ),
+        ],
+      ),
+
+      // Edit and logout buttons
+      Column(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          // Edit
+          ezButton(
+            action: () => Navigator.of(context).popAndPushNamed(profileSettingsRoute),
+            body: ezIcon(PlatformIcons(context).edit),
+          ),
+
+          // Logout
+          ezButton(
+            action: () => logout(context),
+            body: ezIcon(Icons.logout),
+          ),
+        ],
+      ),
+    ],
   );
+}
+
+/// Custom drawer body for Signal Board
+List<Widget> signalDrawerBody(BuildContext context) {
+  double buttonSpacer = AppConfig.prefs[buttonSpacingKey];
+  return [
+    // Add new signal
+    ezIconButton(
+      action: () => Navigator.of(context).popAndPushNamed(createSignalRoute),
+      message: 'New',
+      icon: ezIcon(PlatformIcons(context).add),
+    ),
+    Container(height: buttonSpacer),
+
+    // GoTo settings
+    ezIconButton(
+      action: () => Navigator.of(context).popAndPushNamed(
+        settingsRoute,
+        arguments: {indexArg: 1},
+      ),
+      message: 'Settings',
+      icon: ezIcon(PlatformIcons(context).settings),
+    ),
+    Container(height: buttonSpacer),
+
+    // Show input rules
+    ezButton(
+      action: () => ezDialog(
+        context: context,
+        title: 'Input rules',
+        content: Text(
+          validatorRule,
+          style: getTextStyle(dialogContentStyleKey),
+          textAlign: TextAlign.center,
+        ),
+      ),
+      body: Text('Input rules'),
+    ),
+    Container(height: buttonSpacer),
+  ];
 }
