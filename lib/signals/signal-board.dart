f1f7f96af8eae55e6be53e17a8b4218abb398833
empathictech <105748722+Empathictech@users.noreply.github.com>
2023-03-25 18:40:34 -0400

    definted drawer body

--- lib/signals/signal-board.dart
+++ lib/signals/signal-board.dart
@@ -1,13 +1,15 @@
-import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
-
+import '../app/settings-screen.dart';
 import '../utils/drawers.dart';
 import '../utils/constants.dart';
+import '../utils/validate.dart';
 import '../signals/signal.dart';
 import '../signals/signal-api.dart';
+import '../signals/create-signal-screen.dart';
 import 'package:empathetech_flutter_ui/empathetech_flutter_ui.dart';
 
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
 
 class SignalBoard extends StatefulWidget {
   const SignalBoard({Key? key}) : super(key: key);
@@ -17,6 +19,8 @@ class SignalBoard extends StatefulWidget {
 }
 
 class _SignalBoardState extends State<SignalBoard> {
+  double buttonSpacer = AppConfig.prefs[buttonSpacingKey];
+
   late Stream<QuerySnapshot<Object?>> _signalStream;
   late Stream<QuerySnapshot<Object?>> _requestStream;
 
@@ -25,6 +29,17 @@ class _SignalBoardState extends State<SignalBoard> {
     super.initState();
     _signalStream = streamSignals(membersPath);
     _requestStream = streamSignals(memberReqsPath);
+  }
+
+  void refresh() {
+    setState(doNothing);
+  }
+
+  void reload() {
+    setState(() {
+      _signalStream = streamSignals(membersPath);
+      _requestStream = streamSignals(memberReqsPath);
+    });
   }
 
   @override
@@ -100,17 +115,54 @@ class _SignalBoardState extends State<SignalBoard> {
 
       // User interaction
       drawerHeader: signalDrawerHeader(context),
-      drawerBody: signalDrawerBody(context),
+      drawerBody: [
+        Container(height: buttonSpacer),
+
+        // Add new signal
+        EZButton.icon(
+          action: () async {
+            bool shouldReload = await popAndPushScreen(
+              context: context,
+              screen: CreateSignalScreen(),
+            );
+
+            if (shouldReload) reload();
+          },
+          message: 'New signal',
+          icon: ezIcon(PlatformIcons(context).add),
+        ),
+        Container(height: buttonSpacer),
+
+        // GoTo settings
+        EZButton.icon(
+          action: () => popAndPushScreen(
+            context: context,
+            screen: SettingsScreen(startIndex: 1),
+          ),
+          message: 'Settings',
+          icon: ezIcon(PlatformIcons(context).settings),
+        ),
+        Container(height: buttonSpacer),
+
+        // Show input rules
+        EZButton(
+          action: () => ezDialog(
+            context: context,
+            title: 'Input rules',
+            content: Text(
+              validatorRule,
+              style: getTextStyle(dialogContentStyleKey),
+              textAlign: TextAlign.center,
+            ),
+          ),
+          body: Text('Input rules'),
+        ),
+        Container(height: buttonSpacer),
+      ],
 
       fab: EZButton(
-        // Refresh
-        action: () => setState(doNothing),
-
-        // Reload
-        longAction: () => setState(() {
-          _signalStream = streamSignals(membersPath);
-          _requestStream = streamSignals(memberReqsPath);
-        }),
+        action: refresh,
+        longAction: reload,
         body: ezIcon(PlatformIcons(context).refresh),
         customStyle: ElevatedButton.styleFrom(shape: CircleBorder()),
       ),
